c9296c221cca6f63093936bdeffca026f430eb9f
mustafaalrawy-fedev <154825887+mustafaalrawy-fedev@users.noreply.github.com>
2025-07-31 15:07:32 +0300

    images.js

--- src/data/images.js
+++ src/data/images.js
@@ -4,7 +4,7 @@ import addIcon from '../assets/icons/Add_round_duotone.svg';
 import closeIcon from '../assets/icons/close_ring_duotone-1.svg';
 import trashIcon from '../assets/icons/Trash.svg';
 import rightMarkIcon from '../assets/icons/Done_round.svg';
-import inProgIcon from '../assets/icons/TIme_atack_duotone.svg';
+import inProgIcon from '../assets/icons/Time_atack_duotone.svg';
 import completedIcon from '../assets/icons/Done_round_duotone.svg';
 import wontDoIcon from '../assets/icons/close_ring_duotone.svg';
 
